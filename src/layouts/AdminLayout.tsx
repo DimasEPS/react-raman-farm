c0e38d875b831f8f8bb47611e3aa04238c18e2f5
kite1412 <frozradit@gmail.com>
2025-07-21 08:27:45 +0800

    feat(admin): add menus to nav bar

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,4 +1,8 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react"
+import Data from "../assets/admin-data.svg?react"
+import Add from "../assets/admin-tambah.svg?react"
+import Statistic from "../assets/admin-statisik.svg?react"
+import Account from "../assets/admin-akun.svg?react"
 
 export default function AdminLayout({
   children
@@ -28,13 +32,52 @@ function Header() {
   )
 }
 
+// TODO use Context
 function NavBar() {
+  const [currentMenu, setCurrentMenu] = useState<Menu>("data")
+
   return (
     <div className={`
       flex justify-around w-full ${layoutComponentHeight} items-center bg-charcoal
-      fixed bottom-0 text-white
+      fixed bottom-0
     `}>
-      Menus
+      {
+        menus.map(m => (
+          <div 
+            className={`
+              select-none cursor-pointer ${currentMenu === m.name ? "text-primary" : "text-white"}
+              transition-colors duration-400
+            `}
+            onClick={() => setCurrentMenu(m.name)}
+          >
+            {m.icon}
+          </div>
+        ))
+      }
     </div>
   )
 }
+
+type Menu = "data" | "tambah" | "statistik" | "akun"
+
+const menus: Array<{ 
+  name: Menu
+  icon: JSX.Element
+}> = [
+  {
+    name: "data",
+    icon: <Data />
+  },
+  {
+    name: "tambah",
+    icon: <Add />
+  },
+  {
+    name: "statistik",
+    icon: <Statistic />
+  },
+  {
+    name: "akun",
+    icon: <Account />
+  }
+] 
